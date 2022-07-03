9e1049dc9ed8749630806599fc9b351c79bd6c18
sidoux <sidoux@pop-os.localdomain>
2022-07-03 21:25:03 +0200

    pb avec le tableau example ligne 60 : too many indices for array: array is 2-dimensional, but 3 were indexed

--- Demo_projet.py
+++ Demo_projet.py
@@ -13,8 +13,9 @@ from sklearn.model_selection import train_test_split
 from math import *
 from sklearn.model_selection import GridSearchCV
 import pickle
+import sys
 
-IMAGE_REDUCED_SIZE = 50
+IMAGE_REDUCED_SIZE = 28
 FOLDER = './images/'
 FILENAME = '2model.dat'
 
@@ -41,8 +42,12 @@ if __name__ == "__main__":
     # print(new_list = list(set(my_list))
 
     dataInputs = []
-    for i in range(200):
+    for i in range(1, 2000):
+
+        print(f'{str(i)} : {filename}')
+
         filename = filenames[i]
+
         # print(filename.replace(FOLDER, '').split('_')[0])
         classes.append(filename.replace(FOLDER, '').split('_')[0])
 
@@ -51,16 +56,16 @@ if __name__ == "__main__":
             example, (IMAGE_REDUCED_SIZE, IMAGE_REDUCED_SIZE))
 
         # showImage(example)
-
+        # print(sys.getsizeof(dataInputs))
         line = example[:, :, 0].reshape(1, -1)
         dataInputs.append(line[0])
-
+        example = []
         # print(line.shape)
         # print(example[:, :, 0])
 
     #example = line.reshape(IMAGE_REDUCED_SIZE, IMAGE_REDUCED_SIZE, -1)
     # showImage(example)
-
+    print("LET'S BEGIIIIN")
     dataInputs = np.array(dataInputs)
     print(len(classes))
     print(len(dataInputs))
@@ -68,37 +73,38 @@ if __name__ == "__main__":
         train_desired, test_desired = train_test_split(
             dataInputs, classes, test_size=0.20)
     # print(dataInputs)
-    # mlp = MLPClassifier(activation='tanh', alpha=0.0001,
-    #                     hidden_layer_sizes=(8,), learning_rate='constant', max_iter=500)  # A vous !
+    mlp = MLPClassifier(max_iter=500, learning_rate_init=.007,
+                        verbose=True)  # A vous !
     # --------------------------------------------------------------------------------------
-    # mlp.fit(train_inputs, train_desired)  # Apprentissage
-    parameter_space = {
-        'hidden_layer_sizes': [
-            (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (11,
-                                                                          ), (12,), (13,), (14,), (15,), (16,), (17,), (18,), (19,), (20,), (21,)
-        ],
-        'activation': ['tanh', 'relu', 'identity', 'logistic'],
-        # 'solver': ['sgd', 'adam', 'lbfgs', 'sgd'],
-        'alpha': [0.0001, 0.05],
-        'learning_rate': ['constant', 'adaptive'],
-    }
-    clf = GridSearchCV(MLPClassifier(), parameter_space, cv=3,
-                       scoring='accuracy')
-    clf.fit(train_inputs, train_desired)  # Apprentissage
-    print("Best parameters set found on development set:")
-    print(clf.best_params_)
-    train_score = clf.score(train_inputs, train_desired)
-    test_score = clf.score(test_inputs, test_desired)
+    mlp.fit(train_inputs, train_desired)  # Apprentissage
+    # parameter_space = {
+    #     'hidden_layer_sizes': [
+    #         (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (11,
+    #                                                                       ), (12,), (13,), (14,), (15,), (16,), (17,), (18,), (19,), (20,), (21,)
+    #     ],
+    #     'activation': ['tanh', 'relu', 'identity', 'logistic'],
+    #     # 'solver': ['sgd', 'adam', 'lbfgs', 'sgd'],
+    #     'alpha': [0.0001, 0.05],
+    #     'learning_rate': ['constant', 'adaptive'],
+    # }
+    # clf = GridSearchCV(MLPClassifier(), parameter_space, cv=3,
+    #                    scoring='accuracy')
+    # clf.fit(train_inputs, train_desired)  # Apprentissage
+    # print("Best parameters set found on development set:")
+    # print(clf.best_params_)
+    train_score = mlp.score(train_inputs, train_desired)
+    test_score = mlp.score(test_inputs, test_desired)
     # Score d'apprentissage de régression
     print(f'Training score : {train_score * 100:.2f}%')
     print(f'Test score     : {test_score * 100:.2f}%')  # Score de test
     with open(FILENAME, 'wb') as file:
-        pickle.dump(clf, file)
-    # print(clf.loss_)
-    print(clf.predict([test_inputs[0]]))
-    print(clf.predict_proba([test_inputs[0]]))
+        pickle.dump(mlp, file)
+    print(mlp.loss_)
+    print(test_inputs[0])
+    print(mlp.predict([test_inputs[0]]))
+    print(mlp.predict_proba([test_inputs[0]]))
     fig, ax = plt.subplots()
-    ax.plot(clf.loss_curve_)
+    ax.plot(mlp.loss_curve_)
     plt.yscale('log')
     plt.show()
 
