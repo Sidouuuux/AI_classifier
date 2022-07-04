40873fa2850df2708ef6585dfbae91ed4b19a099
Sid-Ahmed_MIR_Unblocked <smir@unblocked-group.com>
2022-07-05 00:51:49 +0200

    factoring and optimizing the code

--- Demo_projet.py
+++ Demo_projet.py
@@ -6,6 +6,9 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
 import numpy as np
+
+from tqdm import tqdm
+
 from sklearn.neural_network import MLPRegressor
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
@@ -15,9 +18,11 @@ from sklearn.model_selection import GridSearchCV
 import pickle
 import sys
 
-IMAGE_REDUCED_SIZE = 28
+IMAGE_REDUCED_SIZE = 72
 FOLDER = './images/'
-FILENAME = '2model.dat'
+FILENAME = '500model.dat'
+# max 7390
+DATA_SIZE = 1000
 
 # For example purpose only
 # example = example[:, :, 0] #keep only R component (from RGB)
@@ -31,104 +36,85 @@ def showImage(image):
     plt.show()
 
 
-if __name__ == "__main__":
+def getData(_image_folder=FOLDER, _reduce_to=IMAGE_REDUCED_SIZE, _data=DATA_SIZE):
     filenames = []
     classes = []
-    for filename in os.listdir(FOLDER):
+    data = []
+    numpy_file = f'data_{str(_image_folder[2:-1])}_{str(_data)}_{str(_reduce_to)}.npy'
+    print(numpy_file)
+    for filename in os.listdir(_image_folder):
         if filename.endswith('.jpg'):
-            filenames.append(FOLDER + filename)
-            # classes.append(filename.split('_')[0])
-    # print(list(set(classes)))
-    # print(new_list = list(set(my_list))
+            filenames.append(_image_folder + filename)
 
-    dataInputs = []
-    print(len(filenames))
-    for i in range(len(filenames)):
+    if os.path.isfile(numpy_file):
+        for i in tqdm(range(_data)):
+            filename = filenames[i]
+            classes.append(filename.replace(_image_folder, '').split('_')[0])
+        data = np.load(numpy_file)
+        # print(len(classes))
+        # print(set(classes))
+        # print(len(set(classes)))
+        return data, classes
 
-        print(f'{str(i)} : {filename}')
+    else:
+        for i in tqdm(range(_data)):
+            filename = filenames[i]
+            classes.append(filename.replace(_image_folder, '').split('_')[0])
 
-        filename = filenames[i]
+            example = skimage.io.imread(filename)
+            example = skimage.transform.resize(
+                example, (_reduce_to, _reduce_to, 3))
+            # showImage(example)
+            line = example[:, :, 0].reshape(1, -1)
+            data.append(line[0])
 
-        # print(filename.replace(FOLDER, '').split('_')[0])
-        classes.append(filename.replace(FOLDER, '').split('_')[0])
+    data = np.array(data)
+    np.save(numpy_file, data)
+    # data = load('data.npy')
+    return data, classes
 
-        example = skimage.io.imread(filename)
-        example = skimage.transform.resize(
-            example, (IMAGE_REDUCED_SIZE, IMAGE_REDUCED_SIZE,3))
 
-        # showImage(example)
-        print(type(example))
-        print(example.shape)
-        line = example[:, :, 0].reshape(1, -1)
-        dataInputs.append(line[0])
-        example = []
-        # print(line.shape)
-        # print(example[:, :, 0])
+if __name__ == "__main__":
 
-    #example = line.reshape(IMAGE_REDUCED_SIZE, IMAGE_REDUCED_SIZE, -1)
-    # showImage(example)
-    print("LET'S BEGIIIIN")
-    dataInputs = np.array(dataInputs)
+    dataInputs, classes = getData()
+    print(len(dataInputs))
+    print(len(classes))
     # print(len(classes))
     # print(len(dataInputs))
     train_inputs, test_inputs, \
         train_desired, test_desired = train_test_split(
             dataInputs, classes, test_size=0.20)
     # print(dataInputs)
-    mlp = MLPClassifier(max_iter=500, learning_rate_init=.004)  # A vous !
+    # mlp = MLPClassifier(max_iter=500, learning_rate_init=.1)  # A vous !
     # --------------------------------------------------------------------------------------
-    mlp.fit(train_inputs, train_desired)  # Apprentissage
-    # parameter_space = {
-    #     'hidden_layer_sizes': [
-    #         (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (11,
-    #                                                                       ), (12,), (13,), (14,), (15,), (16,), (17,), (18,), (19,), (20,), (21,)
-    #     ],
-    #     'activation': ['tanh', 'relu', 'identity', 'logistic'],
-    #     # 'solver': ['sgd', 'adam', 'lbfgs', 'sgd'],
-    #     'alpha': [0.0001, 0.05],
-    #     'learning_rate': ['constant', 'adaptive'],
-    # }
-    # clf = GridSearchCV(MLPClassifier(), parameter_space, cv=3,
-    #                    scoring='accuracy')
-    # clf.fit(train_inputs, train_desired)  # Apprentissage
-    # print("Best parameters set found on development set:")
-    # print(clf.best_params_)
-    train_score = mlp.score(train_inputs, train_desired)
-    test_score = mlp.score(test_inputs, test_desired)
+    # mlp.fit(train_inputs, train_desired)  # Apprentissage
+    parameter_space = {
+        'hidden_layer_sizes': [
+            (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (11,
+                                                                          ), (12,), (13,), (14,), (15,), (16,), (17,), (18,), (19,), (20,), (21,)
+        ],
+        'activation': ['tanh', 'relu', 'identity', 'logistic'],
+        # 'solver': ['sgd', 'adam', 'lbfgs', 'sgd'],
+        'alpha': [0.0001, 0.05],
+        'learning_rate': ['constant', 'adaptive'],
+    }
+    clf = GridSearchCV(MLPClassifier(), parameter_space, cv=3,
+                       scoring='accuracy')
+    clf.fit(train_inputs, train_desired)  # Apprentissage
+    print("Best parameters set found on development set:")
+    print(clf.best_params_)
+    train_score = clf.score(train_inputs, train_desired)
+    test_score = clf.score(test_inputs, test_desired)
     # Score d'apprentissage de régression
     print(f'Training score : {train_score * 100:.2f}%')
     print(f'Test score     : {test_score * 100:.2f}%')  # Score de test
     with open(FILENAME, 'wb') as file:
-        pickle.dump(mlp, file)
+        pickle.dump(clf, file)
     # print(mlp.loss_)
     # print(test_inputs[0])
-    print(mlp.predict([test_inputs[0]]))
-    print(mlp.predict_proba([test_inputs[0]]))
+    print(clf.predict([test_inputs[0]]))
+    print(clf.predict_proba([test_inputs[0]]))
     fig, ax = plt.subplots()
-    ax.plot(mlp.loss_curve_)
+    ax.plot(clf.loss_curve_)
     plt.yscale('log')
     plt.show()
-
-    # print(mlp.predict([test_inputs[0]]))
-    # print(mlp.predict_proba([test_inputs[0]]))
-    # showSample(test_inputs[0])  # Affichage d'un échantillon
-
-    #print(confusion_matrix(classTrue, classPred))
-    # --------------------------------------------------------------------------------------
-
-    # --- DEMO AUTOENCODER ---
-    # autoEncoder = MLPRegressor(max_iter=2000, hidden_layer_sizes=(100, ))
-    # autoEncoder.fit(dataInputs, dataInputs)
-
-    # fig, ax = plt.subplots()
-    # ax.plot(autoEncoder.loss_curve_)
-    # plt.yscale('log')
-    # plt.show()
-
-    # example = skimage.io.imread('./images/Abyssinian_1.jpg')
-    # example = skimage.transform.resize(
-    #     example, (IMAGE_REDUCED_SIZE, IMAGE_REDUCED_SIZE))
-    # example = example.reshape(1, -1)
-    # result = autoEncoder.predict(example)
-    # result = result.reshape(IMAGE_REDUCED_SIZE, IMAGE_REDUCED_SIZE, -1)
-    # showImage(result)
